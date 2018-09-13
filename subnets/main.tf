3765de59bca2e629df23c7de809b26c2ce8a7038
Yandy Ramirez <yandyr@gmail.com>
2018-09-13 15:41:04 -0400

    code refactor

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,8 +1,9 @@
 data "aws_availability_zones" "azs" {}
 
 locals {
-  subnet_count = "${(var.create_subnet && length(var.ipv4_subnets) > 0) && (length(var.ipv6_cidr_subnets) >= length(var.ipv4_subnets)) ?
-                    length(var.ipv4_subnets) : (var.create_subnet) ? var.num_subnets : 0}"
+  subnet_count = "${(var.create_subnet && length(var.ipv4_subnets) > 0) ?
+                    length(var.ipv4_subnets) : var.create_subnet &&
+                            var.num_subnets > 0 ? var.num_subnets : 0}"
 }
 
 resource "random_shuffle" "random_az" {
@@ -32,11 +33,9 @@ resource "aws_subnet" "this" {
 
   cidr_block = "${length(var.ipv4_subnets) > 0 ?
                   element(coalescelist(var.ipv4_subnets, list("")), count.index) :
-                  cidrsubnet(var.ipv4_cidr_block, var.ipv4_newbits,
-                             var.ipv4_netnum + count.index)}"
+                  cidrsubnet(var.ipv4_cidr_block, var.ipv4_newbits, var.ipv4_netnum + count.index)}"
 
-  ipv6_cidr_block = "${length(var.ipv6_cidr_subnets) > 0 ?
+  ipv6_cidr_block = "${length(var.ipv6_cidr_subnets) > 0 && length(var.ipv6_cidr_subnets) >= local.subnet_count  ?
                       element(coalescelist(var.ipv6_cidr_subnets, list("")), count.index) :
-                      cidrsubnet(var.ipv6_cidr_block, var.ipv6_newbits,
-                                  var.ipv6_netnum + count.index)}"
+                      cidrsubnet(var.ipv6_cidr_block, var.ipv6_newbits, var.ipv6_netnum + count.index)}"
 }
