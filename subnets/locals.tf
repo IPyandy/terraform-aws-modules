9710bac46b7e646a96e5239d453f1971709d60a1
Yandy Ramirez <yandyr@gmail.com>
2018-09-14 18:36:37 -0400

    moved locals to it's own file, otherwise it crashes extension

---
+++ subnets/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  subnet_count = "${(var.create_subnet && length(var.ipv4_subnets) > 0) ?
+                    length(var.ipv4_subnets) : var.create_subnet &&
+                            var.num_subnets > 0 ? var.num_subnets : 0}"
+}
