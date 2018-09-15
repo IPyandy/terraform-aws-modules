6f11d249555a7ef4d5f09163d9619b8fac7c5eb3
Yandy Ramirez <yandyr@gmail.com>
2018-09-14 23:06:56 -0400

    forgot local variable was moved to a file and formatted to 80w line

--- subnets/main.tf
+++ subnets/main.tf
@@ -9,12 +9,6 @@
 
 data "aws_availability_zones" "azs" {}
 
-locals {
-  subnet_count = "${(var.create_subnet && length(var.ipv4_subnets) > 0) ?
-                    length(var.ipv4_subnets) : var.create_subnet &&
-                            var.num_subnets > 0 ? var.num_subnets : 0}"
-}
-
 resource "random_shuffle" "random_az" {
   input        = ["${data.aws_availability_zones.azs.names}"]
   result_count = "${length(data.aws_availability_zones.azs.names)}"
@@ -28,14 +22,17 @@ resource "aws_subnet" "this" {
   tags                            = "${var.tags}"
 
   availability_zone = "${length(var.azs) > 0 ?
-                          element(coalescelist(var.azs, list("")), count.index) :
-                          random_shuffle.random_az.result[count.index]}"
+                        element(coalescelist(var.azs, list("")), count.index) :
+                        random_shuffle.random_az.result[count.index]}"
 
   cidr_block = "${length(var.ipv4_subnets) > 0 ?
-                  element(coalescelist(var.ipv4_subnets, list("")), count.index) :
-                  cidrsubnet(var.ipv4_cidr_block, var.ipv4_newbits, var.ipv4_netnum + count.index)}"
+                  element(coalescelist(var.ipv4_subnets, list("")),
+                  count.index) : cidrsubnet(var.ipv4_cidr_block,
+                  var.ipv4_newbits, var.ipv4_netnum + count.index)}"
 
-  ipv6_cidr_block = "${length(var.ipv6_cidr_subnets) > 0 && length(var.ipv6_cidr_subnets) >= local.subnet_count  ?
-                      element(coalescelist(var.ipv6_cidr_subnets, list("")), count.index) :
-                      cidrsubnet(var.ipv6_cidr_block, var.ipv6_newbits, var.ipv6_netnum + count.index)}"
+  ipv6_cidr_block = "${length(var.ipv6_cidr_subnets) > 0 &&
+                      length(var.ipv6_cidr_subnets) >= local.subnet_count  ?
+                      element(coalescelist(var.ipv6_cidr_subnets, list("")),
+                      count.index) : cidrsubnet(var.ipv6_cidr_block,
+                      var.ipv6_newbits, var.ipv6_netnum + count.index)}"
 }
