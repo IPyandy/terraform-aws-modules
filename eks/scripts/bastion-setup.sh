c4186b62e95e8be32504754a36e191ed8f7a78f7
Yandy Ramirez <yandyr@gmail.com>
2019-01-24 17:36:55 -0500

    update config to match new eks version

--- eks/scripts/bastion-setup.sh
+++ eks/scripts/bastion-setup.sh
@@ -5,12 +5,13 @@ sudo yum install python3 python3-tools -y
 sudo yum install git -y
 sudo yum install zsh -y
 sudo yum install bash-completion -y
-curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/linux/amd64/kubectl
+curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/linux/amd64/kubectl
 sudo mv kubectl /usr/local/bin/
 sudo chmod +x /usr/local/bin/kubectl
-curl -o heptio-authenticator-aws https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/heptio-authenticator-aws
-chmod +x heptio-authenticator-aws
-sudo mv heptio-authenticator-aws /usr/local/bin/heptio-authenticator-aws
+curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/linux/amd64/aws-iam-authenticator
+chmod +x aws-iam-authenticator
+sudo mv aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
+sudo ln -sf /usr/local/bin/aws-iam-authenticator /usr/local/bin/heptio-authenticator-aws
 mkdir -pv $HOME/.kube
 git clone https://github.com/IPyandy/dotfiles-linux.git $HOME/dotfiles
 source $HOME/dotfiles/bootstrap.sh
