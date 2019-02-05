0e178dd06831defe0146a009c0171d8226c37016
Yandy Ramirez <yandyr@gmail.com>
2019-02-05 13:18:30 -0500

    no need to check for istio on cluster termination

--- examples/eks/hack/run.sh
+++ examples/eks/hack/run.sh
@@ -91,82 +91,11 @@ function deployClusters() {
 	fi
 }
 
-function deleteIstio() {
-	echo "Removing istio install from: $(kubectl config current-context)"
-
-	helm ls --all | grep istio
-	if [ $? == 0 ]; then
-		# delete helm install
-		helm delete --debug --purge istio
-
-		# delete customresourcedefs
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "adapters.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "apikeys.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "attributemanifests.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "authorizations.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "bypasses.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "checknothings.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "circonuses.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "deniers.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "destinationrules.networking.istio.i"o
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "edges.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "envoyfilters.networking.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "fluentds.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "gateways.networking.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "handlers.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "httpapispecbindings.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "httpapispecs.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "instances.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "kubernetesenvs.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "kuberneteses.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "listcheckers.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "listentries.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "logentries.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "memquotas.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "metrics.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "noops.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "opas.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "prometheuses.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "quotas.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "quotaspecbindings.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "quotaspecs.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "rbacconfigs.rbac.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "rbacs.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "redisquotas.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "reportnothings.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "rules.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "servicecontrolreports.config.istio."io
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "servicecontrols.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "serviceentries.networking.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "servicerolebindings.rbac.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "serviceroles.rbac.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "signalfxs.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "solarwindses.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "stackdrivers.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "statsds.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "stdios.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "templates.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "tracespans.config.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "virtualservices.networking.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "clusterissuers.certmanager.k8s.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "issuers.certmanager.k8s.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "certificates.certmanager.k8s.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "meshpolicies.authentication.istio.io"
-		kubectl delete customresourcedefinitions.apiextensions.k8s.io "policies.authentication.istio.io"
-		kubectl delete namespace istio-system
-	else
-		echo "Istio not installed, nothing to delete"
-	fi
-}
-
 function destroyCluster() {
 	echo "Destroying -> $PWD"
 	getClusterName
 	kubectl config use-context $CLUSTERNAME
 
-	if [ "$?" == 0 ]; then
-		deleteIstio
-	fi
 	rm "${KUBEDIR}/config"
 	rm "${KUBEDIR}/${CLUSTERNAME}.yaml"
 	terraform destroy -force
