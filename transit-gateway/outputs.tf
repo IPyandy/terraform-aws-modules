8f9a54d95f9302f439548126ae4b31eb4d81ac4f
Yandy Ramirez <yandyr@gmail.com>
2019-05-18 13:31:49 +0000

    feat: code for transit gateway module works

--- transit-gateway/outputs.tf
+++ transit-gateway/outputs.tf
@@ -1,17 +1,17 @@
 # Transit Gateway Outputs
 
 output "transit_gateway" {
-  value = aws_ec2_transit_gateway[0].this
+  value = aws_ec2_transit_gateway.this[0]
 }
 
 output "transit_gateway_id" {
-  value = aws_ec2_transit_gateway[0].this.id
+  value = aws_ec2_transit_gateway.this[0].id
 }
 
 output "default_route_table_id" {
-  value = aws_ec2_transit_gateway[0].this.association_default_route_table_id
+  value = aws_ec2_transit_gateway.this[0].association_default_route_table_id
 }
 
 output "transit_gateway_owner_id" {
-  value = aws_ec2_transit_gateway[0].this.owner_id
+  value = aws_ec2_transit_gateway.this[0].owner_id
 }
